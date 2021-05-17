55e950518763364cc9d744e86fcb142a0ce3086c
Robert Ciliberto <robert.ciliberto@gmail.com>
2021-05-17 17:18:01 -0400

    Add beginnings of Projects page component

---
+++ src/components/Project.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ name }}</h1>
+  <h2>{{ description }}</h2>
+  <img src="{{ imagePath }}" alt="{{ name }}">
+
+</template>
+
+<script>
+export default {
+  name: "Project",
+  props: {
+    name: String,
+    description: String,
+    imagePath: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
