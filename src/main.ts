0ea4bd44e8737cba4201d2ed42ff694733373a51
Robert Ciliberto <robert.ciliberto@gmail.com>
2022-05-26 21:59:37 -0400

    Restart project using Svelte

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021. Robert Ciliberto
- */
-
-import { createApp } from "vue";
-import App from "@/App.vue";
-import router from "./router";
-
-createApp(App).use(router).mount("#app");
--- src/shims-png.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021. Robert Ciliberto
- */
-
-/* eslint-disable */
-declare module "*.png" {
-  const value: string;
-  export default value;
-}
