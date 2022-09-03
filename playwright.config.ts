026d55e43137b12f52bacf150190e9ae7989f00d
Robby Ciliberto <robby@ciliber.to>
2022-09-03 17:25:33 -0400

    Restart project using newer version of sveltekit
    Signed-off-by: Robby Ciliberto <robby@ciliber.to>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,9 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 3000
+		port: 4173
 	}
 };
 
